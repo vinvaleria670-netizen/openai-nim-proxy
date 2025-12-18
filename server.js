bbd373198ed7cc3d752c8c66639a06980c5bf578
vinvaleria670-netizen <vinvaleria670@gmail.com>
2025-12-18 23:09:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const MODEL_MAPPING = {
   'gpt-4-turbo': 'deepseek-v3_2',
 'gpt-4o': 'deepseek-ai/deepseek-v3.1-terminus',
   'claude-3-opus': 'deepseek-ai/deepseek-v3.2',
-  'claude-3-sonnet': 'openai/gpt-oss-20b',
+  'claude-3-sonnet': 'deepseek-ai/deepseek-v3.2',
   'gemini-pro': 'qwen/qwen3-next-80b-a3b-thinking' 
 };
 
