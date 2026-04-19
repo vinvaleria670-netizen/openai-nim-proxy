9e7515b1457403ba1f2339094db56f70d71fd69c
vinvaleria670-netizen <vinvaleria670@gmail.com>
2026-04-20 00:47:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const ENABLE_THINKING_MODE = false; // Set to true to enable chat_template_kwarg
 // Model mapping (adjust based on available NIM models)
 const MODEL_MAPPING = {
   'gpt-3.5-turbo': 'z-ai/glm5',
-  'gpt-4': 'qwen/qwen3-coder-480b-a35b-instruct',
+  'gpt-4': 'z-ai/glm-5.1',
   'gpt-4-turbo': 'deepseek-v3_2',
 'gpt-4o': 'deepseek-ai/deepseek-v3.1-terminus',
   'claude-3-opus': 'z-ai/glm4.7',
